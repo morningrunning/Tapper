9b51a4b85539a39c0b194fd5494fe0b9f4d8f7ed
Ronald Morgan <rmorgan01@gmail.com>
2016-06-28 21:47:00 -0400

    Tapper App
    Basic Tap Game for iOS

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var tapsLbl: UILabel!
+    @IBOutlet weak var tapsBtn: UIButton!
+    
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+        logoImg.hidden = true
+        playBtn.hidden = true
+        howManyTapsTxt.hidden = true
+        
+        tapsLbl.hidden = false
+        tapsBtn.hidden = false
+        
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+        }
+        
+        
+        }
+    
 
 }
 
